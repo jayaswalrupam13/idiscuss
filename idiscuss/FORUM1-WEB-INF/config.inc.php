1c56d13ba2bca4f790045cf64e7b36d7a052b3ae
jayaswalrupam13 <108920860+jayaswalrupam13@users.noreply.github.com>
2022-07-08 13:05:51 +0530

    Update config.inc.php

--- idiscuss/FORUM1-WEB-INF/config.inc.php
+++ idiscuss/FORUM1-WEB-INF/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 $DB_TYPE     = "mysql";
-$DB_NAME     = "idiscuss";
+$DB_NAME     = "##";
 $DB_HOST     = "localhost";
-$DB_USERNAME = "rupam";
-$DB_PASSWORD = "rupam";
+$DB_USERNAME = "##";
+$DB_PASSWORD = "###";
 $CHARSET     = "utf8mb4";
 $PHPINI		 = "C:/xampp/php/php.ini";
 
@@ -36,10 +36,10 @@ $BOOTSTRAP_CSS_URL    = $HTDOCS_URL."/bootstrap-5.1.3-dist/css/bootstrap.min.css
 $BOOTSTRAP_JS_URL     = $HTDOCS_URL."/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js";
 
 //google captcha
-$G_CAPTCHA_CLIENT_KEY = "6Leg36seAAAAAM8r-xr3yLDUY3t6N359x_bWUvaj";
-$G_CAPTCHA_SECRET_KEY = "6Leg36seAAAAALXB5dSRNuMysdFNPIkvS7TRR9hY";
-$G_CAPTCHA_JS         = "https://www.google.com/recaptcha/api.js";
-$G_CAPTCHA_API_URL    = "https://www.google.com/recaptcha/api/siteverify?";
+$G_CAPTCHA_CLIENT_KEY = "#";
+$G_CAPTCHA_SECRET_KEY = "#";
+$G_CAPTCHA_JS         = "#";
+$G_CAPTCHA_API_URL    = "#";
 
 $WHATSAPP_NUM = '917555430191';
 define("DEFAULT_LANG", "en-us");
